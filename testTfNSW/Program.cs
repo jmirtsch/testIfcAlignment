cd0875ed1fb43b5f88d76b3d6dfd5a45b7b30c99
Jon Mirtschin <jonm@geometrygym.com>
2020-11-12 22:04:16 +1100

    Update RC2

--- testTfNSW/Program.cs
+++ testTfNSW/Program.cs
@@ -25,14 +25,12 @@ namespace testTfNSW
 			IfcGeometricRepresentationSubContext axisSubContext = db.Factory.SubContext(IfcGeometricRepresentationSubContext.SubContextIdentifier.Axis);
 
 			IfcAlignment alignment = new IfcAlignment(railway) { Name = "TfNSW Alignment", GlobalId = "1F78QPlVv6N9AnnF$LSkp$" };
-			//IfcCartesianPoint alignmentOrigin = new IfcCartesianPoint(db, 1000, 2000, 3000);
 			IfcCartesianPoint alignmentOrigin = new IfcCartesianPoint(db, 0, 0, 0);
+			//IfcCartesianPoint alignmentOrigin = new IfcCartesianPoint(db, 1000, 2000, 3000);
 			IfcObjectPlacement alignmentPlacement = alignment.ObjectPlacement = new IfcLocalPlacement(new IfcAxis2Placement3D(alignmentOrigin));
 
-			IfcAlignmentHorizontal alignmentHorizontal = new IfcAlignmentHorizontal(alignment);
-			alignmentHorizontal.GlobalId = "0sEEGBFgr289x9s$R$T7N9";
-			alignmentHorizontal.ObjectPlacement = new IfcLocalPlacement(alignmentPlacement, db.Factory.XYPlanePlacement);
-
+		
+			
 			double lineLength = 100;
 			IfcCartesianPoint point1 = new IfcCartesianPoint(db, -lineLength, 0);
 			IfcCartesianPoint point2 = new IfcCartesianPoint(db, 0, 0);
@@ -53,7 +51,7 @@ namespace testTfNSW
 
 			double cx = Math.Sin(thiRadians) * radius;
 			double cy = Math.Cos(thiRadians) * radius;
-			IfcCartesianPoint arcCentre = new IfcCartesianPoint(db, cx, cy);
+			IfcCartesianPoint arcCentre = new IfcCartesianPoint(db, xc - cx, yc + cy);
 
 			double arcLength = 100;
 			double arcAngle = arcLength / radius * 180 / Math.PI;
@@ -62,70 +60,68 @@ namespace testTfNSW
 
 			IfcLine xLine = new IfcLine(db.Factory.Origin2d, new IfcVector(new IfcDirection(db, 1, 0), 1));
 			
-			IfcCurveSegment curveSegment = new IfcCurveSegment(IfcTransitionCode.CONTINUOUS, db.Factory.Origin2dPlace, lineLength, xLine);
-			
-			IfcAlignmentHorizontalSegment linearSegment = new IfcAlignmentHorizontalSegment(alignmentHorizontal, curveSegment, 0, 0, lineLength, IfcAlignmentHorizontalSegmentTypeEnum.LINE) { PredefinedType = IfcAlignmentHorizontalSegmentTypeEnum.LINE };
-			IfcAxis2Placement2D axis2Placement2D = new IfcAxis2Placement2D(point1);
-			compositeSegments.Add(new IfcCurveSegment(IfcTransitionCode.CONTINUOUS, axis2Placement2D, lineLength, xLine));
-			linearSegment.ObjectPlacement = new IfcLocalPlacement(alignmentHorizontal.ObjectPlacement, axis2Placement2D);
-			new IfcPropertySet(linearSegment, "PSet_AlignmentHorizontalSegmentCommon",
-				new IfcPropertySingleValue(db, "StartDirection", new IfcPlaneAngleMeasure(0))
-				); 
+			IfcAlignmentHorizontalSegment linearSegment = new IfcAlignmentHorizontalSegment(point1, 0, 0, 0, lineLength, IfcAlignmentHorizontalSegmentTypeEnum.LINE);
+			IfcLine line = new IfcLine(point1, new IfcVector(db.Factory.XAxis, 1));
+			IfcTrimmedCurve trimmedCurve = new IfcTrimmedCurve(line, new IfcTrimmingSelect(0, point1), new IfcTrimmingSelect(lineLength, point2), true, IfcTrimmingPreference.PARAMETER);
+			compositeSegments.Add(new IfcCompositeCurveSegment(IfcTransitionCode.CONTINUOUS, true, trimmedCurve));
 
 			List<double> coefficientsX = new List<double>() { 0, 1, 0, 0, 0, -0.9 * m * m, 0, 0, 0, 5.175 * Math.Pow(m,4), 0, 0, 0, -43.1948 * Math.Pow(m,6), 0, 0, 0, 426.0564 * Math.Pow(m,8)};
 			List<double> coefficientsY = new List<double>() { 0, 0, 0, m, 0, 0, 0, -2.7 * Math.Pow(m,3), 0, 0, 0, 17.955 * Math.Pow(m,5), 0, 0, 0, -158.258 * Math.Pow(m,7), 0, 0, 0, 1604.338 * Math.Pow(m,9)};
 			IfcSeriesParameterCurve seriesParameterCurve = new IfcSeriesParameterCurve(db.Factory.Origin2dPlace, coefficientsX, coefficientsY);
-			curveSegment = new IfcCurveSegment(IfcTransitionCode.CONTSAMEGRADIENTSAMECURVATURE, db.Factory.Origin2dPlace, transitionLength, seriesParameterCurve);
 
-			IfcAlignmentHorizontalSegment transitionSegment = new IfcAlignmentHorizontalSegment(alignmentHorizontal, curveSegment, 0, radius, transitionLength, IfcAlignmentHorizontalSegmentTypeEnum.NONLINEAR) { ObjectType = "TfNSW" };
-			compositeSegments.Add(new IfcCurveSegment(IfcTransitionCode.CONTSAMEGRADIENTSAMECURVATURE, db.Factory.Origin2dPlace, transitionLength, seriesParameterCurve));
-			transitionSegment.ObjectPlacement = new IfcLocalPlacement(alignmentHorizontal.ObjectPlacement, db.Factory.XYPlanePlacement);
-			new IfcPropertySet(transitionSegment, "PSet_AlignmentHorizontalSegmentCommon",
-							new IfcPropertySingleValue(db, "StartDirection", new IfcPlaneAngleMeasure(0)),
-							new IfcPropertySingleValue(db, "IsStartRadiusCCW", new IfcBoolean(true)),
-							new IfcPropertySingleValue(db, "IsEndRadiusCCW", new IfcBoolean(true))
-							);
+			IfcAlignmentHorizontalSegment transitionSegment = new IfcAlignmentHorizontalSegment(point2, 0, 0, radius, transitionLength, IfcAlignmentHorizontalSegmentTypeEnum.CUBICSPIRAL);// { ObjectType = "TfNSW" };
+			trimmedCurve = new IfcTrimmedCurve(seriesParameterCurve, new IfcTrimmingSelect(0, point2), new IfcTrimmingSelect(transitionLength, point3), true, IfcTrimmingPreference.PARAMETER);
+			compositeSegments.Add(new IfcCompositeCurveSegment(IfcTransitionCode.CONTSAMEGRADIENTSAMECURVATURE, true, trimmedCurve));
 
-			new IfcPropertySet(transitionSegment, "TfNSW_Transition", new IfcPropertySingleValue(db, "m", m));
-
-			IfcAxis2Placement2D circlePlacement = new IfcAxis2Placement2D(new IfcCartesianPoint(db, 0, radius)) { RefDirection = new IfcDirection(db, 0,-1) };
+			IfcAxis2Placement2D circlePlacement = new IfcAxis2Placement2D(arcCentre) { RefDirection = new IfcDirection(db, Math.Sin(thiRadians), -Math.Cos(thiRadians)) };
 			IfcCircle circle = new IfcCircle(circlePlacement, radius);
-			curveSegment = new IfcCurveSegment(IfcTransitionCode.CONTSAMEGRADIENTSAMECURVATURE, db.Factory.Origin2dPlace, arcLength, circle);
+			trimmedCurve = new IfcTrimmedCurve(circle, new IfcTrimmingSelect(0, point3), new IfcTrimmingSelect(arcLength / radius * 180 / Math.PI), true, IfcTrimmingPreference.PARAMETER);
 		
-			IfcAlignmentHorizontalSegment arcSegment = new IfcAlignmentHorizontalSegment(alignmentHorizontal, curveSegment, radius, radius, arcLength, IfcAlignmentHorizontalSegmentTypeEnum.ARC);
-			axis2Placement2D = new IfcAxis2Placement2D(point3) { RefDirection = new IfcDirection(db, Math.Cos(thiRadians), Math.Sin(thiRadians)) };
-			compositeSegments.Add(new IfcCurveSegment(IfcTransitionCode.CONTSAMEGRADIENTSAMECURVATURE, axis2Placement2D, arcLength, circle));
-			arcSegment.ObjectPlacement = new IfcLocalPlacement(alignmentHorizontal.ObjectPlacement,axis2Placement2D);
-			new IfcPropertySet(arcSegment, "PSet_AlignmentHorizontalSegmentCommon",
-								new IfcPropertySingleValue(db, "StartDirection", new IfcPlaneAngleMeasure(thi)),
-								new IfcPropertySingleValue(db, "IsCCW", new IfcBoolean(true))
-								);
+			IfcAlignmentHorizontalSegment arcSegment = new IfcAlignmentHorizontalSegment(point3, thi, radius, radius, arcLength, IfcAlignmentHorizontalSegmentTypeEnum.CIRCULARARC);
+			compositeSegments.Add(new IfcCompositeCurveSegment(IfcTransitionCode.CONTSAMEGRADIENTSAMECURVATURE,true, trimmedCurve));
 
-			IfcCompositeCurve horizontalCurve = new IfcCompositeCurve(compositeSegments);
-			alignmentHorizontal.Representation = new IfcProductDefinitionShape(new IfcShapeRepresentation(axisSubContext, horizontalCurve, ShapeRepresentationType.Curve2D));
+			IfcCompositeCurve alignmentCurve = new IfcCompositeCurve(compositeSegments);
+			double startDist = -123;
+			IfcAlignmentHorizontal alignmentHorizontal = new IfcAlignmentHorizontal(alignment, new List<IfcAlignmentHorizontalSegment>())
+			{
+				StartDistAlong = startDist,
+			};
+			alignmentHorizontal.GlobalId = "0sEEGBFgr289x9s$R$T7N9";
+			alignmentPlacement = alignmentHorizontal.ObjectPlacement = new IfcLocalPlacement(alignmentPlacement, db.Factory.XYPlanePlacement);
+			alignmentHorizontal.Representation = new IfcProductDefinitionShape(new IfcShapeRepresentation(axisSubContext, alignmentCurve, ShapeRepresentationType.Curve2D));
 
-			IfcAlignmentVertical alignmentVertical = new IfcAlignmentVertical(alignment);
-			IfcDistanceExpression verticalDistanceExpression = new IfcDistanceExpression(0, horizontalCurve);
+			IfcAlignmentSegment alignmentSegment = new IfcAlignmentSegment(alignmentHorizontal, transitionSegment);
+			alignmentSegment.GlobalId = "2_crD$eoPDah_QvgsPhXF3";
+			alignmentSegment.ObjectType = "TFNSW";
+			new IfcPropertySet(alignmentSegment, "TfNSW_Transition", new IfcPropertySingleValue(db, "m", m));
+		
+
+			IfcPointByDistanceExpression verticalDistanceExpression = new IfcPointByDistanceExpression(0, alignmentCurve);
 			double startHeight = 25;
 			verticalDistanceExpression.OffsetVertical = startHeight;
-			IfcLinearAxis2Placement verticalAxisPlacement = new IfcLinearAxis2Placement(verticalDistanceExpression);
-			IfcLinearPlacement verticalLinearPlacement = new IfcLinearPlacement(alignmentPlacement, verticalAxisPlacement);
-			alignmentVertical.ObjectPlacement = verticalLinearPlacement;
+			IfcAlignmentVerticalSegment verticalSegment = new IfcAlignmentVerticalSegment(db, startDist, lineLength + transitionLength + arcLength, startHeight, 0.01, IfcAlignmentVerticalSegmentTypeEnum.CONSTANTGRADIENT);
+			
+			IfcAlignmentVertical alignmentVertical = new IfcAlignmentVertical(alignment, verticalSegment);
+			alignmentVertical.GlobalId = "2YR0TUxTv75RC2XxZVmlj8";
 
-			List<IfcCurveSegment> verticalSegments = new List<IfcCurveSegment>();
-			IfcLine linearGradient = new IfcLine(db.Factory.Origin, new IfcVector(new IfcDirection(db, 1, 0, 0.01), 1)); //not right and should it be xy or xz
-			IfcCurveSegment verticalCurveSegment = new IfcCurveSegment(IfcTransitionCode.CONTINUOUS, new IfcAxis2Placement3D(db.Factory.Origin2d), 50, linearGradient);
-			verticalSegments.Add(verticalCurveSegment);
-			IfcAlignmentVerticalSegment verticalSegment = new IfcAlignmentVerticalSegment(alignmentVertical,
-				verticalLinearPlacement, verticalCurveSegment, 0, 50, startHeight, 0.01, 
-				IfcAlignmentVerticalSegmentTypeEnum.LINE);
+			//IfcLinearAxis2Placement verticalAxisPlacement = new IfcLinearAxis2Placement(verticalDistanceExpression);
+			//IfcLinearPlacement verticalLinearPlacement = new IfcLinearPlacement(alignmentPlacement, verticalAxisPlacement);
+			//alignmentVertical.ObjectPlacement = verticalLinearPlacement;
 
-			IfcGradientCurve gradientCurve = new IfcGradientCurve(horizontalCurve, verticalSegments);
-			alignment.Representation = new IfcProductDefinitionShape(new IfcShapeRepresentation(axisSubContext, gradientCurve, ShapeRepresentationType.Curve3D));
-			IfcDistanceExpression distanceExpression = new IfcDistanceExpression(150, horizontalCurve);
+			//List<IfcCurveSegment> verticalSegments = new List<IfcCurveSegment>();
+			//IfcLine linearGradient = new IfcLine(db.Factory.Origin, new IfcVector(new IfcDirection(db, 1, 0, 0.01), 1)); //not right and should it be xy or xz
+			//IfcCurveSegment verticalCurveSegment = new IfcCurveSegment(IfcTransitionCode.CONTINUOUS, new IfcAxis2Placement3D(db.Factory.Origin2d), 50, linearGradient);
+			//verticalSegments.Add(verticalCurveSegment);
+			//IfcAlignmentVerticalSegment verticalSegment = new IfcAlignmentVerticalSegment(alignmentVertical,
+			//	verticalLinearPlacement, verticalCurveSegment, 0, 50, startHeight, 0.01, 
+			//	IfcAlignmentVerticalSegmentTypeEnum.LINE);
+
+			//IfcGradientCurve gradientCurve = new IfcGradientCurve(horizontalCurve, verticalSegments);
+			//alignment.Representation = new IfcProductDefinitionShape(new IfcShapeRepresentation(axisSubContext, gradientCurve, ShapeRepresentationType.Curve3D));
+			IfcPointByDistanceExpression distanceExpression = new IfcPointByDistanceExpression(150, alignmentCurve);
 			distanceExpression.OffsetLateral = 5;
-			IfcLinearAxis2Placement axis2Placement = new IfcLinearAxis2Placement(distanceExpression);
-			IfcLinearPlacement linearPlacement = new IfcLinearPlacement(alignmentHorizontal.ObjectPlacement, axis2Placement);
+			IfcAxis2PlacementLinear axis2PlacementLinear = new IfcAxis2PlacementLinear(distanceExpression);
+			IfcLinearPlacement linearPlacement = new IfcLinearPlacement(alignmentPlacement , axis2PlacementLinear);
 			IfcExtrudedAreaSolid extrudedAreaSolid = new IfcExtrudedAreaSolid(new IfcRectangleProfileDef(db, "", 0.5, 0.5), 5);
 			IfcProductDefinitionShape productDefinitionShape = new IfcProductDefinitionShape(new IfcShapeRepresentation(extrudedAreaSolid));
 			IfcPile pile = new IfcPile(railway, linearPlacement, productDefinitionShape);
